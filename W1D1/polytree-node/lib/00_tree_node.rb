b60e5dfc8685535ee74b779f9e706826464b629d
squeaky-bruce <bruce@squeaky.com>
2020-02-26 16:50:37 -0500

    DFS done

--- W1D1/polytree-node/lib/00_tree_node.rb
+++ W1D1/polytree-node/lib/00_tree_node.rb
@@ -24,4 +24,29 @@ class PolyTreeNode
         raise "Error: #{child_node.value} is not a child of this node" if !@children.include?(child_node)
         child_node.parent = nil
     end
+
+    def dfs(target_value)
+        return nil if self == nil
+        return self if self.value == target_value
+        @children.each do |child|
+            search_result = child.dfs(target_value)
+            return search_result unless search_result == nil
+        end
+
+        nil
+    end
+
+    def get_children
+        @children.map {|child| child.value}
+    end
 end
+
+n1 = PolyTreeNode.new(1)
+n2 = PolyTreeNode.new(2)
+n2a = PolyTreeNode.new("2a")
+n3 = PolyTreeNode.new(3)
+
+n1.add_child(n2)
+n1.add_child(n3)
+n2.add_child(n2a)
+p n1.dfs("4")
