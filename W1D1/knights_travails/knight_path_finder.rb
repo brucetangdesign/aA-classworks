b20a74bb147c8a44f6fcd62d19b1bce53626d13d
brucetangdesign <bruce@brucetangdesign.com>
2020-02-28 16:10:32 -0500

    Added initialize, build_move_tree to Kinight's Travails

--- W1D1/knights_travails/knight_path_finder.rb
+++ W1D1/knights_travails/knight_path_finder.rb
@@ -1,7 +1,15 @@
 require_relative "poly_tree_node.rb"
 
 class KnightPathFinder
+    attr_reader :root_node
+
     def initialize(position)
+        @root_node = PolyTreeNode.new(position)
+        build_move_tree
+    end
+
+    def build_move_tree
+
     end
 end
 
