81c8e074d33d5c23f4b2c8eb93ebfc0f6a6a543f
brucetangdesign <bruce@brucetangdesign.com>
2020-02-29 15:31:32 -0500

    added build_move_tree to Knights Travails

--- W1D1/knights_travails/knight_path_finder.rb
+++ W1D1/knights_travails/knight_path_finder.rb
@@ -10,7 +10,23 @@ class KnightPathFinder
     end
 
     def build_move_tree
-
+        start_pos = @root_node
+        
+        queue = []
+        queue << start_pos
+        100.times do #until queue.empty?
+            search_result = queue.shift
+            if search_result != nil
+            new_move_positions(search_result.value).each do | position |
+                if position != search_result.value
+                    new_node = PolyTreeNode.new(position)
+                    search_result.add_child(new_node)
+                    queue << new_node
+                end
+            end
+            end
+        end
+        p @considered_positions
     end
 
     def self.valid_moves(position)
@@ -19,24 +35,28 @@ class KnightPathFinder
         row, col = position
 
         start_row = row > 1 ? row - 2 : row
-        end_row = row + 2 < (grid_size-1) ? row + 2 : start_row
+        end_row = row + 2 < (grid_size-1) ? row + 2 : row
         start_col = col > 0 ? col - 1 : col + 1
 
-        (start_row..(row+2)).step(2).each do |cur_row|
-            if cur_row != row
-                #find one left and one right
-                    valid_moves << [cur_row,col-1] if col > 0
-                    valid_moves << [cur_row,col+1] if col < grid_size-1
-            else
-                # in same row we move over 2 cols and up and down 1 row
-                if row > 0
-                    valid_moves << [row-1,col-2] if col > 1
-                    valid_moves << [row-1,col+2] if col < grid_size - 2
-                end
+        if start_row < grid_size
+            (start_row..end_row).step(2).each do |cur_row|
+                if cur_row != row
+                    #find one left and one right
+                        valid_moves << [cur_row,col-1] if col-1 > 0
+                        valid_moves << [cur_row,col+1] if col+1 < grid_size-1
+                else
+                    col_before = col - 2
+                    col_after = col + 2
+                    # in same row we move over 2 cols and up and down 1 row
+                    if row - 1 > 0
+                        valid_moves << [row-1,col_before] if col_before > 1
+                        valid_moves << [row-1,col_after] if col_after < grid_size - 1
+                    end
 
-                if row < grid_size-2
-                    valid_moves << [row+1,col-2] if col > 1
-                    valid_moves << [row+1,col+2] if col < grid_size - 2
+                    if row + 1 <= grid_size-1
+                        valid_moves << [row+1,col_before] if col_before > 1
+                        valid_moves << [row+1,col_after] if col_after < grid_size - 1
+                    end
                 end
             end
         end
@@ -44,9 +64,10 @@ class KnightPathFinder
     end
 
     def new_move_positions(position)
-        p @considered_positions.concat(KnightPathFinder.valid_moves(position).reject{ |move| @considered_positions.include?(move) })
+        new_positions = KnightPathFinder.valid_moves(position).reject{ |move| @considered_positions.include?(move) }
+        @considered_positions.concat(new_positions)
+        new_positions
     end
 end
 
 kpf = KnightPathFinder.new([0, 0])
-kpf.new_move_positions([0,0])
