a6c85887503e2f721ab13acb6a79153656f1d976
brucetangdesign <bruce@brucetangdesign.com>
2020-03-02 11:11:39 -0500

    set up instance variables, wrote children method for Tic Tac Toe Node

--- W1D1/tic-tac-toe/lib/tic_tac_toe_node.rb
+++ W1D1/tic-tac-toe/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,16 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    (0...@board.rows.length).each do |row_n|
+      (0...@board.rows[row_n].length).each do |col_n|
+        if @board.empty?([row_n,col_n])
+          mark = (@next_mover_mark == :x)? :o : :x
+          @board.rows[row_n][col_n] = TicTacToeNode.new(@board, mark, [row_n,col_n])
+        end
+      end
+    end
   end
 end
+
+t = TicTacToeNode.new(Board.new,:x)
+p t.children
