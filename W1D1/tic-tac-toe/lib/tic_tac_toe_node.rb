0da270b313e1b4fe7ff8a51b67385877ad84fbc4
brucetangdesign <bruce@brucetangdesign.com>
2020-03-07 16:46:04 -0500

    COmpleted tic tac toe node

--- W1D1/tic-tac-toe/lib/tic_tac_toe_node.rb
+++ W1D1/tic-tac-toe/lib/tic_tac_toe_node.rb
@@ -9,27 +9,19 @@ class TicTacToeNode
     @prev_move_pos = prev_move_pos
   end
 
-  def losing_node?(evaluator)
-  end
-
-  def winning_node?(evaluator)
-  end
-
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
     child_nodes = []
     (0...@board.rows.length).each do |row_n|
       (0...@board.rows[row_n].length).each do |col_n|
-        if @board.empty?([row_n,col_n])
-          child_mark = (@next_mover_mark == :x)? :o : :x
+        pos = [row_n,col_n]
+        if @board.empty?(pos)
           child_board = @board.dup
-          child_board.rows.each_with_index do |row,row_n|
-            row.each_with_index do |col,col_n|
-              child_board.rows[row_n][col_n] = child_mark
-            end
-          end
-          child_nodes << TicTacToeNode.new(child_board, child_mark, [row_n,col_n])
+          child_board[pos] = @next_mover_mark
+          child_mark = (@next_mover_mark == :x)? :o : :x
+          
+          child_nodes << TicTacToeNode.new(child_board, child_mark, pos)
         end
       end
     end
@@ -38,11 +30,22 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    return @board.won? && @board.winner != evaluator if @board.over?
+    
+    if @next_mover_mark == evaluator 
+      self.children.all? { |node| node.losing_node?(evaluator) }
+    else
+      self.children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 
-  def wining_node?(evaluator)
+  def winning_node?(evaluator)
+    return @board.winner == evaluator if @board.over?
+
+    if @next_mover_mark == evaluator
+      self.children.any? { |node| node.winning_node?(evaluator) }
+    else
+      self.children.all? { |node| node.winning_node?(evaluator) }
+    end
   end
 end
-
-t = TicTacToeNode.new(Board.new,:x)
-t.children
