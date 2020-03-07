60fc317c3f85cb3b1856404949b525096ee37d01
brucetangdesign <bruce@brucetangdesign.com>
2020-03-07 16:46:24 -0500

    Started Super CPU

--- W1D1/tic-tac-toe/lib/super_computer_player.rb
+++ W1D1/tic-tac-toe/lib/super_computer_player.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe_node'
 
+#In the #move method, build a TicTacToeNode from the board stored in the game passed in as an argument. Next, iterate through the children of the node we just created.
+
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    
   end
 end
 
