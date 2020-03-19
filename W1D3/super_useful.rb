5a0e375806e40326457d685538ef72f082143ab1
brucetangdesign <bruce@brucetangdesign.com>
2020-03-19 15:19:43 -0400

    Fixed errors in super_useful and user_script

--- W1D3/super_useful.rb
+++ W1D3/super_useful.rb
@@ -1,30 +1,53 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str) 
+  rescue ArgumentError => e
+    puts "Error:  #{e.inspect}"
+  ensure
+    return nil
+  end 
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
+class CoffeeError < StandardError
+end
+
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  else
+    if maybe_fruit == "coffee"
+      raise CoffeeError
+    else
+      raise StandardError
+    end
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue CoffeeError => e
+    puts "Thanks for the coffee but try something different"
+    retry
+  rescue StandardError => e
+     puts "Sorry, I dont eat that"
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise "Sorry, you need to know each other for 5 years or more to be considered best friends!" if yrs_known < 5
+    raise "Provide a name" if name.empty?
+    raise "Provide a pastime" if fav_pastime.empty?
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
