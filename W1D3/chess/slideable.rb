884dea8055825ed25ddd37a6b40b54f73bd69fb1
brucetangdesign <bruce@brucetangdesign.com>
2020-03-26 10:50:28 -0400

    Added slideable module

---
+++ W1D3/chess/slideable.rb
@@ -0,0 +1,20 @@
+module Slideable
+    def moves
+
+    end
+
+    def horizontal_dirs
+
+    end
+
+    def diagonal_dirs
+        
+    end
+
+    private
+    @@HORIZONTAL_DIRS = []
+    @@DIAGONAL_DIRS = []
+    def grow_unblocked_mpves(dx,dy)
+
+    end
+end
